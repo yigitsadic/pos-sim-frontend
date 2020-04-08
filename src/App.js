49605b8c643af8a68f5ff63d17522fe466f71864
Yiğit Sadıç <yigit.sadic@globalmiles.com>
2020-04-08 22:07:41 +0300

    Installed graphql hooks for development.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,6 +11,10 @@ import {
 import { Sales } from "./components/Sales";
 import { Products } from "./components/Products";
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+
 const Header = styled.h1`
   text-align: center;
   font-size: 2rem;
@@ -17,32 +23,34 @@ const Header = styled.h1`
 
 export const App = () => {
   return (
-    <Router>
-      <Header>Hello React!</Header>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header>Hello React!</Header>
 
-      <ul>
-        <li>
-          <Link to="/sales">Sales</Link>
-        </li>
+        <ul>
+          <li>
+            <Link to="/sales">Sales</Link>
+          </li>
 
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-      </ul>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
 
-      <Switch>
-        <Route path="/sales">
-          <Sales />
-        </Route>
+        <Switch>
+          <Route path="/sales">
+            <Sales />
+          </Route>
 
-        <Route path="/products">
-          <Products />
-        </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
 
-        <Route path="/">
-          <Sales />
-        </Route>
-      </Switch>
-    </Router>
+          <Route path="/">
+            <Sales />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
+
+const PRODUCTS = gql`
+{
+  products {
+    _id
+    name
+    price
+  }
+}
+`;
 
 export const Products = () => {
+  const { loading, error, data } = useQuery(PRODUCTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return <React.Fragment>
     Hello this is Products
+
+    {data.products.map(({ _id, name, price }) => {
+    return <div key={_id}>
+      {name}: ₺{price}
+    </div>
+  })}
   </React.Fragment>
 }
