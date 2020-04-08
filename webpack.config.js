1563b1b808e720c29c2a9bfe7523e7d5c77d8b78
Yiğit Sadıç <yigit.sadic@globalmiles.com>
2020-04-08 22:18:54 +0300

    Installed normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
       }
     ]
   },
